04f1581e0f4da8fed4ac1cb9759a582546282fdb
Erick-pdx <francoer@pdx.edu>
2024-12-05 21:03:25 -0800

    Formatted files, fix small screen issue, html and css checker, renamed images, added js functions for popups

--- properties.js
+++ properties.js
@@ -1,7 +1,7 @@
-const showResumeButtons = document.querySelectorAll(".download-resume");
+const showResumeButtons = document.querySelectorAll(".preview-resume");
 
 for (const showResumeButton of showResumeButtons) {
-    showResumeButton.addEventListener("click", (event) => {
+  showResumeButton.addEventListener("click", (event) => {
     const popupSection = event.currentTarget.parentElement.nextElementSibling;
     popupSection.style.display = "block";
   });
@@ -10,9 +10,27 @@ for (const showResumeButton of showResumeButtons) {
 const closeResumeButtons = document.querySelectorAll(".close-popup-button");
 
 for (const closeResumeButton of closeResumeButtons) {
-    closeResumeButton.addEventListener("click", (event) => {
+  closeResumeButton.addEventListener("click", (event) => {
     console.log(event.target);
-    const popupSection = event.currentTarget.parentElement.parentElement.parentElement;
+    const popupSection =
+      event.currentTarget.parentElement.parentElement.parentElement;
     popupSection.style.display = "none";
   });
 }
+
+function downloadResume() {
+  const link = document.createElement("a");
+  link.href = "/images/Erick Franco's Resume (1).pdf";
+  link.download = "Erick Franco's Resume.pdf";
+  link.click();
+}
+
+const contactForm = document.getElementById("Contact");
+contactForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  alert("Form submitted successfully!");
+});
+
+contactForm.addEventListener("reset", () => {
+  alert("Form reset!");
+});
